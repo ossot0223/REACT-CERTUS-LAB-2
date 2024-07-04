ff6c7dd91ae12aed1297ff417392a3c20f9af67d
ossot0223 <168603641+ossot0223@users.noreply.github.com>
2024-07-04 08:08:40 -0500

    app.js

---
+++ app.js
@@ -0,0 +1,10 @@
+const titulo = "primeraaccion";
+  const caracteristicas = ["elemento 1","elemento 2","elemento 3"];
+ 
+ const data = {
+  titulo: 'hola mundo desde un objeto',
+  description: 'descripcion desde un objeto'
+ }
+
+ //Lista//
+ //condiciones//
